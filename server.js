61d54301a18742aa5388c37a4838723be9ba0f33
skonSolo <81214406+olosnoks@users.noreply.github.com>
2025-03-05 21:20:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
     res.send("Do you want a recipe?");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
